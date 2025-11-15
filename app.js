c86750d9cb5523da8ed4c9e18fd32ad3116c8e71
db79430 <db79430@gmail.com>
2025-11-16 01:07:40 +0300

    f

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const CONFIG = {
   TERMINAL_KEY: '1761129018508DEMO',
   SECRET_KEY: 'jDkIojG12VaVNopw',
-  BASE_URL: 'https://securepay.tinkoff.ru/v2/'
+  BASE_URL: 'https://securepay.tinkoff.ru/v2/Init'
 };
 
 console.log('🔧 Server started with TerminalKey:', CONFIG.TERMINAL_KEY);
