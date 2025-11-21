29656d17bd4c21042768bddd1eef862ceab26d5e
db79430 <db79430@gmail.com>
2025-11-21 12:29:09 +0300

    fix token

--- services/TinkoffService.js
+++ services/TinkoffService.js
@@ -180,7 +180,7 @@ class TinkoffService {
         method: 'POST',
         url: url,
         data: requestData,
-        timeout: 30000,
+        timeout: 10000,
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json'
@@ -221,7 +221,7 @@ class TinkoffService {
         method: 'POST',
         url: `${this.baseURL}`,
         data: testData,
-        timeout: 30000
+        timeout: 10000
       });
 
       return {
