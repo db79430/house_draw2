e45eea925ef95fad0b0ddc490b6cf286be8c66f4
db79430 <db79430@gmail.com>
2025-11-26 12:27:21 +0300

    fix payment2

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 // Парсинг данных
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 
 app.use('/api', diagnosticRoutes);
 
