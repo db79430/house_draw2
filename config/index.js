e357fd192e79d36aad1bebf840ec495b0ea91b9a
db79430 <db79430@gmail.com>
2025-11-26 16:44:25 +0300

    fix

--- config/index.js
+++ config/index.js
@@ -14,8 +14,8 @@ const CONFIG = {
     HOST: process.env.DB_HOST,
     PORT: process.env.DB_PORT || 5432,
     NAME: process.env.DB_NAME,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
+    USER: process.env.DB_USER ,
+    PASSWORD: process.env.DB_PASSWORD || 'secure_password_123',
     SSL: true
   },
 
