f075a2a9fd72847926773eaf622de6cc7095570f
db79430 <db79430@gmail.com>
2025-11-28 22:54:52 +0300

    fix

--- services/EmailServices.js
+++ services/EmailServices.js
@@ -3,6 +3,9 @@ import path from 'path';
 import fs from 'fs/promises';
 import { fileURLToPath } from 'url';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
