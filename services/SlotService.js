bee3a62ad31972c57bdbaa06fb63bcbc1b2af792
db79430 <db79430@gmail.com>
2025-12-04 00:57:37 +0300

    fix

--- services/SlotService.js
+++ services/SlotService.js
@@ -46,7 +46,7 @@ class SlotService {
                 TerminalKey: CONFIG.TINKOFF.TERMINAL_KEY,
                 Amount: amount,
                 OrderId: orderId,
-                Description: `Покупка ${slotCount} слота (ов). Член клуба: ${user.memberNumber || 'Не указан'}`,
+                Description: `Покупка ${slotCount} слота (ов). Член клуба: ${user.membership_number || 'Не указан'}`,
                 NotificationURL: `${CONFIG.APP.BASE_URL}/payment-notification`,
                 DATA: {
                     Email: user.email || '',
